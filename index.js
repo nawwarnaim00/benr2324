ce968b392780a1a3df2ca11c81ea101aecce34c2
nawwarnaim00 <b022210013@student.utem.edu.my>
2023-10-24 12:05:14 +0800

    test

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const app = express()
 const port = process.env.PORT || 3000;
 app.use(express.json())
 app.get('/', (req, res) => {
- res.send('Hello World!')
+ res.send('Hello')
 })
 app.listen(port, () => {
  console.log(`Example app listening on port ${port}`)
